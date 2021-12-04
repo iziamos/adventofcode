d7c8cb2951332fdb3e6bb9eaf48dde8cedea2bd9
John Ziamos <iziamos@gmail.com>
2021-12-04 11:51:44 +0000

    read in bingo calls

--- aocj/java/src/Program.java
+++ aocj/java/src/Program.java
@@ -1,5 +1,21 @@
+
+import java.util.Arrays;
+import java.util.List;
+
+import static java.nio.file.Files.lines;
+import static java.nio.file.Paths.get;
+import static java.util.stream.Collectors.toList;
+
 public class Program {
-    public static void main (String... args) {
-        System.out.println("does this run");
+    public static void main (String... args) throws Exception {
+        final List<String> lines = lines(get(args[0])).collect(toList());
+
+        final var linesIterator = lines.iterator();
+
+        final String first = linesIterator.next();
+
+        final String[] bingoCalls = first.split(",");
+
+        System.out.println(Arrays.toString(bingoCalls));
     }
 }
